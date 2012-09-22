3088b1403c4a06ead4f1ad982be5060e4ee3bd57
bpwong <bpwong@berkeley.edu>
2012-09-22 11:50:57 -0700

    Stocks v3: Larger Label Fontsize

--- plotData.py
+++ plotData.py
@@ -19,9 +19,9 @@ f, ax1 = plt.subplots(1,1)
 ax2 = ax1.twinx() 
 ax2.set_ylim([-150,100])
 
-ln1 = ax1.plot(goog['date'],goog['val'], label='Google Stock Value')
-ln2 = ax2.plot(ny['date'],ny['val'], '--', c='r',label = 'NY Mon. High Temp')
-ln3 = ax1.plot(yahoo['date'],yahoo['val'], c='purple',label='Yahoo! Stock Value')
+ln1 = ax1.plot(goog['date'],goog['val'], linewidth=2,label='Google Stock Value')
+ln2 = ax2.plot(ny['date'],ny['val'], 'r--', linewidth=2, label = 'NY Mon. High Temp')
+ln3 = ax1.plot(yahoo['date'],yahoo['val'], c='purple',linewidth=2,label='Yahoo! Stock Value')
 
 # usetex demo: http://matplotlib.org/examples/pylab_examples/usetex_demo.html
 rc('text', usetex = True)
@@ -29,10 +29,10 @@ plt.title(r'\bf{New York, Temperature, Google, and Yahoo!}',fontsize=20)
 
 lns = ln3+ln1+ln2
 labs = [l.get_label() for l in lns]
-ax1.legend(lns,labs,loc='center left',frameon=False,prop={'size':10})
+ax1.legend(lns,labs,loc='center left',frameon=False,prop={'size':11})
 
-ax1.set_xlabel('Date (MJD)')
-ax1.set_ylabel('Value (Dollars)')
+ax1.set_xlabel('Date (MJD)',fontsize=14)
+ax1.set_ylabel('Value (Dollars)',fontsize=14)
 ax1.minorticks_on()
 ax1.tick_params(labelsize=10)
 ax1.set_ylim(auto=True)
@@ -40,7 +40,7 @@ ax1.xaxis.tick_bottom()
 #ax1.splines[''].set_linewidth(10)
 #ax1.set_linewidth(4)
 
-ax2.set_ylabel('Temperature ($\degree$F)')
+ax2.set_ylabel('Temperature ($\degree$F)',fontsize=14)
 ax2.minorticks_on()
 ax2.tick_params(labelsize=10)
 ax2.autoscale(axis='x',tight=True)
